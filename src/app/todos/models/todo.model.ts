395ffa5608b00b1f7e5d49ff7b585993894e729f
axvg <91773368+axvg@users.noreply.github.com>
2023-06-21 21:43:14 -0500

    add todo model

---
+++ src/app/todos/models/todo.model.ts
@@ -0,0 +1,11 @@
+export class Todo{
+    public id: number;
+    public text: string;
+    public isCompleted: boolean;
+
+    constructor(text: string){
+        this.id = new Date().getTime();
+        this.text = text;
+        this.isCompleted = false;
+    }
+}
